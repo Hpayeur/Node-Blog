6326fc904144aff9348c83d052f8f154d113cdfe
Hpayeur <hailpaye017@gmail.com>
2026-02-17 15:58:12 -0700

    Adding the folders

---
+++ app.js
@@ -0,0 +1,17 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const servers = dns.getServers();
+console.log("Node.js is using these DNS servers:", servers);
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
