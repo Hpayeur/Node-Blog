c797107efe270190c854b62b1d50ee0fdf731fe8
Hpayeur <hailpaye017@gmail.com>
2026-02-18 13:30:31 -0700

    Partials Search.ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const Post = require("../models/Post");
 
 //Blog's Home Page
-
 router.get("/", async (req, res) => {
   const locals = {
-    title: "Node,js Blog",
+    title: "NodeJs Blog",
     description:
       " A Blog template application that will be used for your own use.",
   };
   try {
-    const data = (await Post.find()).toSorted({ createdAt: "desc" });
+    const data = await Post.find().sort({ createdAt: "desc" });
+    res.render("index", { locals, data });
   } catch (error) {
     console.error(error);
   }
