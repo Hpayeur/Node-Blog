6a35bd484db19b49000f93ae64edf9ce6f0a547d
Hpayeur <hailpaye017@gmail.com>
2026-02-17 17:13:56 -0700

    main.js

---
+++ server/routes/main.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+//Blog's Home Page
+
+router("/", async (req, res) => {
+  const locals = {
+    title: "Node,js Blog",
+    description:
+      " A Blog template application that will be used for your own use.",
+  };
+  try {
+    const data = (await Post.find()).toSorted({ createdAt: "desc" });
+  } catch (error) {
+    console.error(error);
+  }
+});
