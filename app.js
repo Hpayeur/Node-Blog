a9a3794672508a8a15ef70b495a4f77115df4407
Hpayeur <hailpaye017@gmail.com>
2026-02-19 17:46:30 -0700

    finishing up base for Part 3

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const connectDB = require("./server/config/db");
 connectDB();
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+const session = require("express-session");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,6 +23,19 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  }),
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
