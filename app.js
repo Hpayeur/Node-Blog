562b9399f8bda2575572008a3631af1e524d37b5
Hpayeur <hailpaye017@gmail.com>
2026-02-24 13:29:05 -0700

    App.js

--- app.js
+++ app.js
@@ -4,28 +4,23 @@ const servers = dns.getServers();
 console.log("Node.js is using these DNS servers:", servers);
 
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
 const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
-const connectDB = require("./server/config/db");
-connectDB();
-const cookieParser = require("cookie-parser");
-const MongoStore = require("connect-mongo");
 const methodOverride = require("method-override");
 const session = require("express-session");
 
+const connectDB = require("./server/config/db");
+connectDB();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(expressLayouts);
-app.use(express.static("public"));
-app.set("layout", "./layouts/main");
-app.set("view engine", "ejs");
-
-app.use("/", require("./server/routes/main"));
-app.use("/", require("./server/routes/admin"));
 app.use(cookieParser());
 app.use(methodOverride("_method"));
+
 app.use(
   session({
     secret: "keyboard cat",
@@ -36,6 +31,14 @@ app.use(
     }),
   }),
 );
+
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
