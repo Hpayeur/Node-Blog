8e9cd99b1cb92a464a0a65539726b899733dec85
Hpayeur <hailpaye017@gmail.com>
2026-02-24 19:51:33 -0700

    updating code

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -84,14 +84,18 @@ router.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
+
     try {
-      const user = await User.create({ username, password: hashedPassword });
+      const user = await User.create({
+        username,
+        password: hashedPassword,
+      });
       res.status(201).json({ message: "User created successfully", user });
     } catch (error) {
-      if (error.code === 11000) {
-        res
-          .status(400)
-          .json({ message: "Something went wrong with the server!" });
+      if (error === 11000) {
+        return res
+          .status(500)
+          .json({ message: "Something went wrong with the server" });
       }
     }
   } catch (error) {
